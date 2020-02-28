70ef44ad6296d4a4605a1df8da4db19372a01c72
Jidan-Fauzan <zidanajj2@gmail.com>
2020-02-28 09:12:14 +0700

    (Konversi Jam)

---
+++ Fungsi (Konversi Jam).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int kj(int j,int m,int d)
+{
+    int K;
+    K=(j*360)+(m*60)+d;
+    return k;
+
+}
+int main()
+{
+    int jam2,menit3,detik4;
+
+    cout<<"Masukkan jam 2:";cin>>jam2;
+    cout<<"masukkan menit 3:";cin>>menit3;
+    cout<<"masukkan detik 4:";cin>>detik4;
+
+    cout<<"Jumlah Detik4:"<<kj(jam2,menit3,detik5)<<endl:
+
+    int jam3,menit4,detik5;
+
+    cout<<"masukkan jam 3:";cin>>jam3;
+    cout<<"masukkan menit 4:";cin>>menit4;
+    cout<<"masukkan detik 5:";cin>>detik5;
+
+    cout<<"Jumlah Detik5:"<<kj(jam3,menit4,detik5)<<endl;
+
+
+    int jumlah;
+    jumlah=kj(jam2,menit3,detik4)+kj(jam3,menit4,detik5);
+    cout<<"hasil:"<<jumlah;
+}
